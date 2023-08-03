1e8f9a973d61c42ab79a481349bb4b36cd5c412c
niranjsasikumar <sasikumar.niranj@gmail.com>
2023-08-03 19:57:38 -0400

    Add insertSeasonData function

--- database/populate_database.js
+++ database/populate_database.js
@@ -3,6 +3,13 @@ import mysql from "mysql";
 const currentSeason = 20232024;
 const currentSeasonYear = 2023;
 
+const seasons = [];
+
+for (let year = 1917; year < currentSeasonYear; year++) {
+  const season = parseInt(year.toString() + (year+1).toString());
+  seasons.push(season);
+}
+
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -18,17 +25,17 @@ async function fetchData(url) {
   return data;
 }
 
-// Fetch data of current teams from NHL API and update the data in "teams" table in database
+// Fetch data of current teams from NHL API and overwrite the data in "teams" table
 async function updateTeamsData() {
-  console.log("Start inserting into \"teams\" table");
+  console.log("Start overwriting \"teams\" table");
 
   connection.query("DELETE FROM teams");
-  const data = await fetchData("https://statsapi.web.nhl.com/api/v1/teams");
+  const teamsData = (await fetchData("https://statsapi.web.nhl.com/api/v1/teams")).teams;
 
   const statement = `INSERT INTO teams (ID, Name, LocationName, TeamName, Abbreviation, LightLogoURL, DarkLogoURL, VenueName, VenueCity, FirstYearOfPlay, Conference, Division)
   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
-  for (const team of data.teams) {
+  for (const team of teamsData) {
     const values = [
       team.id,
       team.name,
@@ -47,8 +54,59 @@ async function updateTeamsData() {
     connection.query(statement, values);
   }
 
-  console.log("Finished inserting into \"teams\" table\n");
+  console.log("Finished overwriting \"teams\" table\n");
 }
 
-await updateTeamsData();
+// Fetch data for the given seasons from NHL API and insert or update the data in "seasons" table
+async function insertSeasonData(seasons) {
+  console.log("Start inserting into / updating \"seasons\" table");
+
+  const seasonsData = (await fetchData("https://statsapi.web.nhl.com/api/v1/seasons?season=" + seasons)).seasons;
+
+  for (const season of seasonsData) {
+    const standingsData = (await fetchData("https://statsapi.web.nhl.com/api/v1/standings?season=" + season.seasonId)).records;
+
+    const conferences = new Set();
+    const divisions = [];
+    const playoffRounds = [];
+
+    if (standingsData.length > 1) {
+      for (const division of standingsData) {
+        divisions.push(division.division?.name);
+        if ("conference" in division) conferences.add(division.conference?.name);
+      }
+    }
+
+    // Only add playoff rounds from the 1942-43 season onwards due to missing or inconsistent data for prior seasons
+    if (season.seasonId >= 19421943) {
+      const playoffsData = await fetchData("https://statsapi.web.nhl.com/api/v1/tournaments/playoffs?season=" + season.seasonId);
+      if ("rounds" in playoffsData) playoffsData.rounds.forEach((round) => playoffRounds.push(round.names?.name));
+    }
+
+    const statement = `INSERT INTO seasons (ID, RegularSeasonStartDate, RegularSeasonEndDate, SeasonEndDate, NumberOfGames, TiesInUse, ConferencesInUse, DivisionsInUse, WildCardInUse, Conferences, Divisions, PlayoffRounds)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ON DUPLICATE KEY UPDATE RegularSeasonStartDate=VALUES(RegularSeasonStartDate), RegularSeasonEndDate=VALUES(RegularSeasonEndDate), SeasonEndDate=VALUES(SeasonEndDate), NumberOfGames=VALUES(NumberOfGames), TiesInUse=VALUES(TiesInUse), ConferencesInUse=VALUES(ConferencesInUse), DivisionsInUse=VALUES(DivisionsInUse), WildCardInUse=VALUES(WildCardInUse), Conferences=VALUES(Conferences), Divisions=VALUES(Divisions), PlayoffRounds=VALUES(PlayoffRounds)`;
+
+    const values = [
+      season.seasonId,
+      season.regularSeasonStartDate,
+      season.regularSeasonEndDate,
+      season.seasonEndDate,
+      season.numberOfGames,
+      season.tiesInUse,
+      season.conferencesInUse,
+      season.divisionsInUse,
+      season.wildCardInUse,
+      Array.from(conferences).toString(),
+      divisions.toString(),
+      playoffRounds.toString()
+    ];
+
+    connection.query(statement, values);
+  }
+
+  console.log("Finished inserting into / updating \"seasons\" table");
+}
+
+await insertSeasonData(seasons);
 connection.end();
