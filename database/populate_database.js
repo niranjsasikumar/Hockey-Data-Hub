866f27ddd10d90e74f0e3cff60fd430a3136fbee
niranjsasikumar <sasikumar.niranj@gmail.com>
2023-08-05 19:36:55 -0400

    Add insertPlayerData function

--- database/populate_database.js
+++ database/populate_database.js
@@ -128,15 +128,15 @@ async function insertPlayoffSeriesData(seasons) {
             parseInt(season.toString() + round.number.toString() + series.seriesNumber.toString()),
             season,
             round.number,
-            round.names.name,
+            round.names?.name,
             series.seriesNumber,
-            series.matchupTeams[0].team.id,
-            series.matchupTeams[0].team.name,
-            "https://www-league.nhlstatic.com/images/logos/teams-" + season +"-light/" + series.matchupTeams[0].team.id + ".svg",
-            series.matchupTeams[1].team.id,
-            series.matchupTeams[1].team.name,
-            "https://www-league.nhlstatic.com/images/logos/teams-" + season +"-light/" + series.matchupTeams[1].team.id + ".svg",
-            series.currentGame.seriesSummary.seriesStatus
+            series.matchupTeams[0].team?.id,
+            series.matchupTeams[0].team?.name,
+            "https://www-league.nhlstatic.com/images/logos/teams-" + season +"-light/" + series.matchupTeams[0].team?.id + ".svg",
+            series.matchupTeams[1].team?.id,
+            series.matchupTeams[1].team?.name,
+            "https://www-league.nhlstatic.com/images/logos/teams-" + season +"-light/" + series.matchupTeams[1].team?.id + ".svg",
+            series.currentGame?.seriesSummary?.seriesStatus
           ];
 
           connection.query(statement, values);
@@ -166,32 +166,29 @@ async function insertStandingsData(seasons) {
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         ON DUPLICATE KEY UPDATE TeamID=VALUES(TeamID), Team=VALUES(Team), Season=VALUES(Season), LogoURL=VALUES(LogoURL), Conference=VALUES(Conference), Division=VALUES(Division), ClinchIndicator=VALUES(ClinchIndicator), \`Rank\`=VALUES(\`Rank\`), Points=VALUES(Points), GamesPlayed=VALUES(GamesPlayed), Wins=VALUES(Wins), Losses=VALUES(Losses), Ties=VALUES(Ties), OvertimeLosses=VALUES(OvertimeLosses), GoalsFor=VALUES(GoalsFor), GoalsAgainst=VALUES(GoalsAgainst), Difference=VALUES(Difference), HomeRecord=VALUES(HomeRecord), AwayRecord=VALUES(AwayRecord), Last10=VALUES(Last10), Streak=VALUES(Streak)`;
 
-        const teamId = team.team.id;
-        const leagueRecord = team.leagueRecord;
-
         const values = [
-          parseInt(season.toString() + teamId.toString()),
-          teamId,
-          team.team.name,
+          parseInt(season.toString() + team.team?.id.toString()),
+          team.team?.id,
+          team.team?.name,
           season,
-          "https://www-league.nhlstatic.com/images/logos/teams-" + season +"-light/" + teamId + ".svg",
-          "conference" in division ? division.conference.name : "",
-          "division" in division ? division.division.name : "",
-          "clinchIndicator" in team ? team.clinchIndicator : "",
+          "https://www-league.nhlstatic.com/images/logos/teams-" + season +"-light/" + team.team?.id + ".svg",
+          division.conference?.name,
+          division.division?.name,
+          team.clinchIndicator,
           parseInt(team.divisionRank) ? parseInt(team.divisionRank) : team.leagueRank,
           team.points,
           team.gamesPlayed,
-          leagueRecord.wins,
-          leagueRecord.losses,
-          "ties" in leagueRecord ? leagueRecord.ties : 0,
-          "ot" in leagueRecord ? leagueRecord.ot : 0,
+          team.leagueRecord?.wins,
+          team.leagueRecord?.losses,
+          team.leagueRecord?.ties,
+          team.leagueRecord?.ot,
           team.goalsScored,
           team.goalsAgainst,
           team.goalsScored - team.goalsAgainst,
-          getRecordString(team.records.overallRecords[0]),
-          getRecordString(team.records.overallRecords[1]),
-          getRecordString(team.records.overallRecords[3]),
-          team.streak.streakCode
+          getRecordString(team.records?.overallRecords[0]),
+          getRecordString(team.records?.overallRecords[1]),
+          getRecordString(team.records?.overallRecords[3]),
+          team.streak?.streakCode
         ];
 
         connection.query(statement, values);
@@ -202,5 +199,87 @@ async function insertStandingsData(seasons) {
   console.log("Finished inserting into / updating \"standings\" table");
 }
 
-await insertStandingsData(seasons);
+// Fetch player data for the given seasons from NHL API and insert or update the data in "skaters" and "goalies" tables
+async function insertPlayerData(seasons) {
+  console.log("Start inserting into / updating \"skaters\" and \"goalies\" tables");
+
+  for (const season of seasons) {
+    const teamsData = (await fetchData("https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster,roster.person&season=" + season)).teams;
+
+    for (const team of teamsData) {
+      if (!("roster" in team)) continue;
+
+      for (const player of team.roster?.roster) {
+        let playerStats = (await fetchData("https://statsapi.web.nhl.com/api/v1/people/" + player.person?.id + "/stats?stats=statsSingleSeason&season=" + season)).stats[0].splits;
+        let hasStats = true;
+
+        playerStats.length === 0 ? hasStats = false : playerStats = playerStats[0].stat;
+
+        let statement = "";
+        let values = [
+          parseInt(season.toString() + team.id.toString() + player.person?.id.toString()),
+          player.person?.id,
+          player.person?.fullName,
+          team.id,
+          team.name,
+          season,
+          "https://cms.nhl.bamgrid.com/images/headshots/current/168x168/" + player.person?.id + ".jpg",
+          "https://www-league.nhlstatic.com/images/logos/teams-" + season +"-light/" + team.id + ".svg",
+          player.jerseyNumber,
+          player.person?.captain,
+          player.person?.alternateCaptain,
+          player.person?.shootsCatches,
+          player.person?.nationality,
+          player.person?.birthDate,
+          player.person?.height,
+          player.person?.weight,
+          hasStats ? playerStats.games : null
+        ];
+
+        if (player.position?.type === "Goalie") {
+          statement = `INSERT INTO goalies (ID, PlayerID, Player, TeamID, Team, Season, ImageURL, TeamLogoURL, Number, Captain, AlternateCaptain, Catches, Nationality, DateOfBirth, Height, Weight, GamesPlayed, ShotsAgainst, GoalsAgainst, Saves, SavePercentage, GoalsAgainstAverage, Shutouts)
+          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+          ON DUPLICATE KEY UPDATE PlayerID=VALUES(PlayerID), Player=VALUES(Player), TeamID=VALUES(TeamID), Team=VALUES(Team), Season=VALUES(Season), ImageURL=VALUES(ImageURL), TeamLogoURL=VALUES(TeamLogoURL), Number=VALUES(Number), Captain=VALUES(Captain), AlternateCaptain=VALUES(AlternateCaptain), Catches=VALUES(Catches), Nationality=VALUES(Nationality), DateOfBirth=VALUES(DateOfBirth), Height=VALUES(Height), Weight=VALUES(Weight), GamesPlayed=VALUES(GamesPlayed), ShotsAgainst=VALUES(ShotsAgainst), GoalsAgainst=VALUES(GoalsAgainst), Saves=VALUES(Saves), SavePercentage=VALUES(SavePercentage), GoalsAgainstAverage=VALUES(GoalsAgainstAverage), Shutouts=VALUES(Shutouts)`;
+
+          if (hasStats) {
+            values.push(playerStats.shotsAgainst);
+            values.push(playerStats.goalsAgainst);
+            values.push(playerStats.saves);
+            values.push(playerStats.savePercentage);
+            values.push(playerStats.goalAgainstAverage);
+            values.push(playerStats.shutouts);
+          } else {
+            for (let i = 0; i < 6; i++) values.push(null);
+          }
+        } else {
+          statement = `INSERT INTO skaters (ID, PlayerID, Player, TeamID, Team, Season, ImageURL, TeamLogoURL, Number, Position, PositionType, Captain, AlternateCaptain, Shoots, Nationality, DateOfBirth, Height, Weight, GamesPlayed, Goals, Assists, Points, PointsPerGamesPlayed, PowerPlayGoals, PowerPlayPoints, Shots, ShootingPercentage, FaceoffPercentage)
+          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+          ON DUPLICATE KEY UPDATE PlayerID=VALUES(PlayerID), Player=VALUES(Player), TeamID=VALUES(TeamID), Team=VALUES(Team), Season=VALUES(Season), ImageURL=VALUES(ImageURL), TeamLogoURL=VALUES(TeamLogoURL), Number=VALUES(Number), Position=VALUES(Position), PositionType=VALUES(PositionType), Captain=VALUES(Captain), AlternateCaptain=VALUES(AlternateCaptain), Shoots=VALUES(Shoots), Nationality=VALUES(Nationality), DateOfBirth=VALUES(DateOfBirth), Height=VALUES(Height), Weight=VALUES(Weight), GamesPlayed=VALUES(GamesPlayed), Goals=VALUES(Goals), Assists=VALUES(Assists), Points=VALUES(Points), PointsPerGamesPlayed=VALUES(PointsPerGamesPlayed), PowerPlayGoals=VALUES(PowerPlayGoals), PowerPlayPoints=VALUES(PowerPlayPoints), Shots=VALUES(Shots), ShootingPercentage=VALUES(ShootingPercentage), FaceoffPercentage=VALUES(FaceoffPercentage)`;
+
+          values.splice(9, 0, player.position?.abbreviation, player.position?.type);
+          
+          if (hasStats) {
+            values.push(playerStats.goals);
+            values.push(playerStats.assists);
+            values.push(playerStats.points);
+            values.push((playerStats.points / playerStats.games).toFixed(3));
+            values.push(playerStats.powerPlayGoals);
+            values.push(playerStats.powerPlayPoints);
+            values.push(playerStats.shots);
+            values.push(playerStats.shotPct);
+            values.push(playerStats.faceOffPct);
+          } else {
+            for (let i = 0; i < 9; i++) values.push(null);
+          }
+        }
+
+        connection.query(statement, values);
+      }
+    }
+  }
+
+  console.log("Finished inserting into / updating \"skaters\" and \"goalies\" tables");
+}
+
+await insertPlayerData(seasons);
 connection.end();
