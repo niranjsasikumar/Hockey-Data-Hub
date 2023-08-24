514005a2a7ba05b4dbed9385bf1dd22b51a18e72
niranjsasikumar <sasikumar.niranj@gmail.com>
2023-08-24 18:12:00 -0400

    Add schedule card and sample schedule data

--- frontend/src/components/pages/schedule/Schedule.js
+++ frontend/src/components/pages/schedule/Schedule.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Container, Typography, Divider, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import { Container, Typography, Divider, FormControl, InputLabel, Select, MenuItem, Grid } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
 import { teams } from "./sample-schedule-data";
+import { schedule } from "./sample-schedule-data";
+import ScheduleCard from "./ScheduleCard";
 
 function Schedule() {
   const [team, setTeam] = React.useState("");
@@ -11,12 +13,11 @@ function Schedule() {
   };
 
   return(
-    <Container maxWidth={false} sx={{ maxWidth: "1300px", py: 3 }}>
+    <Container maxWidth={false} sx={{ maxWidth: "1300px", pt: 3, pb: 5 }}>
       <Typography variant="h3" component="h1">Schedule</Typography>
       <Divider sx={{ mt: 2, mb: 3 }} />
-      <DatePicker label="Start Date" views={["year", "month", "day"]} sx={{ mr: 2, mb: 2, width: 230 }} />
-      <DatePicker label="End Date" views={["year", "month", "day"]} sx={{ mr: 2, mb: 2, width: 230 }} />
-      <FormControl sx={{ mb: 2, width: 230 }}>
+      <DatePicker label="Date" views={["year", "month", "day"]} sx={{ mr: 2, mb: 2, width: 230 }} />
+      <FormControl sx={{ mr: 2, mb: 2, width: 230 }}>
         <InputLabel id="team-label">Team</InputLabel>
         <Select labelId="team-label" id="team" value={team} label="Team" onChange={handleTeamChange}>
           {teams.map((team) => (
@@ -25,6 +26,19 @@ function Schedule() {
         </Select>
       </FormControl>
       <Divider sx={{ mt: 1, mb: 3 }} />
+
+      {schedule.map((date) => (
+        <div key={date.date}>
+          <Typography variant="h5" component="h2" mt={4} mb={3}>{date.date}</Typography>
+          <Grid container spacing={3}>
+            {date.games.map((game) => (
+              <Grid item xs={12} sm={6} key={game.id}>
+                <ScheduleCard game={game} />
+              </Grid>
+            ))}
+          </Grid>
+        </div>
+      ))}
     </Container>
   );
 }
