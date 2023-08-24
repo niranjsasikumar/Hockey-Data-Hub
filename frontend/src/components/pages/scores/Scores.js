26bd7c02ea2728cd8dbe5f3727fa96845a20b888
niranjsasikumar <sasikumar.niranj@gmail.com>
2023-08-24 18:24:06 -0400

    Change grid breakpoint on scores page

--- frontend/src/components/pages/scores/Scores.js
+++ frontend/src/components/pages/scores/Scores.js
@@ -14,13 +14,13 @@ function Scores() {
       <Typography variant="h5" component="h2" mt={4} mb={3}>Tue, Jun 13, 2023</Typography>
 
       <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} sm={6}>
           <ScoreCard game={playoffGame} />
         </Grid>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} sm={6}>
           <ScoreCard game={playoffGame} />
         </Grid>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} sm={6}>
           <ScoreCard game={regularGame} />
         </Grid>
       </Grid>
@@ -28,7 +28,7 @@ function Scores() {
       <Typography variant="h5" component="h2" mt={4} mb={3}>Mon, Jun 12, 2023</Typography>
 
       <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} sm={6}>
           <ScoreCard game={playoffGame} />
         </Grid>
       </Grid>
@@ -36,16 +36,16 @@ function Scores() {
       <Typography variant="h5" component="h2" mt={4} mb={3}>Sun, Jun 11, 2023</Typography>
 
       <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} sm={6}>
           <ScoreCard game={regularGame} />
         </Grid>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} sm={6}>
           <ScoreCard game={playoffGame} />
         </Grid>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} sm={6}>
           <ScoreCard game={regularGame} />
         </Grid>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} sm={6}>
           <ScoreCard game={regularGame} />
         </Grid>
       </Grid>
