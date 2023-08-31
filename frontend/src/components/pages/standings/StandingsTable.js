4dc4aa6c81100c1a6544ab1c17d42c2d243afa0f
niranjsasikumar <sasikumar.niranj@gmail.com>
2023-08-31 18:31:52 -0400

    Change style of standings table

--- frontend/src/components/pages/standings/StandingsTable.js
+++ frontend/src/components/pages/standings/StandingsTable.js
@@ -3,11 +3,11 @@ import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody
 function StandingsTable({ standings, season }) {
   return(
     <TableContainer component={Paper} elevation={2} sx={{ border: "1px solid lightgrey", borderBottom: "none", mb: 4 }}>
-      <Table size="small" aria-label="standings table">
+      <Table size="small" aria-label="standings table" sx={{ borderCollapse: "separate" }}>
 
         <TableHead>
           <TableRow>
-            <TableCell sx={{ position: "sticky", left: "0", backgroundColor: "#F7F7F7" }}>Team</TableCell>
+            <TableCell sx={{ position: "sticky", left: "0", backgroundColor: "#fff", borderRight: "1px solid lightgrey" }}>Team</TableCell>
             <TableCell align="center">Pts</TableCell>
             <TableCell align="center">GP</TableCell>
             <TableCell align="center">W</TableCell>
@@ -27,7 +27,19 @@ function StandingsTable({ standings, season }) {
         <TableBody>
           {standings.map((team) => (
             <TableRow key={team.Name}>
-              <TableCell component="th" scope="row" sx={{ whiteSpace: "nowrap", position: "sticky", left: "0", backgroundColor: "#F7F7F7" }}>
+              <TableCell
+              component="th"
+              scope="row"
+              sx={{
+                whiteSpace: "nowrap",
+                position: "sticky",
+                left: "0",
+                backgroundColor: "#fff",
+                borderLeft: team.ClinchIndicator !== null ? "3px solid #160085" : "none",
+                borderRight: "1px solid lightgrey",
+                boxSizing: "border-box"
+              }}
+              >
                 <Stack
                   direction="row"
                   justifyContent="flex-start"
