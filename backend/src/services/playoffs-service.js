ee39b44beb526c55615f7c60e63c6e939e3aa1db
niranjsasikumar <sasikumar.niranj@gmail.com>
2023-09-06 14:54:06 -0400

    Modify playoffs service to include curent season

--- backend/src/services/playoffs-service.js
+++ backend/src/services/playoffs-service.js
@@ -1,6 +1,9 @@
 import connection from "../database/database.js";
+import { fetchDataFromNHLApi } from "../utils/utils.js";
 
 export default async function getPlayoffs(season) {
+  if (season === "current") return await getCurrentSeasonPlayoffs();
+
   const playoffRounds = (await connection.query(
     `SELECT playoffRounds FROM seasons
     WHERE id = ${season}`
@@ -19,6 +22,40 @@ export default async function getPlayoffs(season) {
     playoffs.push({
       round: round,
       series: series
+    });
+  }
+
+  return playoffs;
+}
+
+async function getCurrentSeasonPlayoffs() {
+  const playoffsData = (await fetchDataFromNHLApi("/tournaments/playoffs?expand=round.series,schedule.game.seriesSummary,series.matchup.team")).rounds;
+  const playoffs = [];
+
+  for (const round of playoffsData) {
+    const { number, names } = round;
+    const seriesList = [];
+
+    for (const series of round.series) {
+      const { seriesNumber, matchupTeams, currentGame } = series;
+      const team1 = matchupTeams[0].team;
+      const team2 = matchupTeams[1].team;
+
+      seriesList.push({
+        id: number.toString() + seriesNumber,
+        team1Id: team1.id,
+        team1Abbreviation: team1.abbreviation,
+        team1LogoURL: `https://assets.nhle.com/logos/nhl/svg/${team1.abbreviation}_light.svg`,
+        team2Id: team2.id,
+        team2Abbreviation: team2.abbreviation,
+        team2LogoURL: `https://assets.nhle.com/logos/nhl/svg/${team2.abbreviation}_light.svg`,
+        statusShort: currentGame.seriesSummary?.seriesStatusShort
+      });
+    }
+
+    playoffs.push({
+      round: names.name,
+      series: seriesList
     });
   }
 
