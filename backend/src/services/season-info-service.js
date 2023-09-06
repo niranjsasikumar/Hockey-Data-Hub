17c386d4b25938fadb3683729fe71c464bd25b8a
niranjsasikumar <sasikumar.niranj@gmail.com>
2023-09-06 13:00:33 -0400

    Add current season info for season info service

--- backend/src/services/season-info-service.js
+++ backend/src/services/season-info-service.js
@@ -1,12 +1,32 @@
 import connection from "../database/database.js";
+import { fetchDataFromNHLApi } from "../utils/utils.js";
 
 export default async function getSeasonInfo(season) {
-  const [results] = await connection.query(
+  if (season === "current") return await getCurrentSeasonInfo();
+
+  return (await connection.query(
     `SELECT ${queryColumns} FROM seasons
     WHERE id = ${season}`
-  );
+  ))[0][0];
+}
 
-  return results[0];
+async function getCurrentSeasonInfo() {
+  const seasonInfo = (await fetchDataFromNHLApi("/seasons/current")).seasons[0];
+  const { conferencesInUse, divisionsInUse, tiesInUse } = seasonInfo;
+
+  return {
+    id: "current",
+    conferencesInUse: conferencesInUse,
+    divisionsInUse: divisionsInUse,
+    conferences: null,
+    divisions: null,
+    tiesInUse: tiesInUse,
+    overtimeLossPointInUse: true,
+    powerPlayStatsTracked: true,
+    shootingStatsTracked: true,
+    faceoffStatsTracked: true,
+    saveStatsTracked: true
+  };
 }
 
 const queryColumns = [
