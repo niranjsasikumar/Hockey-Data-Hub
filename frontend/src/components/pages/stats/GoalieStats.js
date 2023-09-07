b362a60bf0813724ba92da067896cd076643afa7
niranjsasikumar <sasikumar.niranj@gmail.com>
2023-09-07 15:28:52 -0400

    Modify stats page to use backend services

--- frontend/src/components/pages/stats/GoalieStats.js
+++ frontend/src/components/pages/stats/GoalieStats.js
@@ -1,6 +1,6 @@
 import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, Stack, Box, Typography } from "@mui/material";
 
-function GoalieStats({ stats }) {
+function GoalieStats({ stats, season }) {
   return(
     <TableContainer component={Paper} elevation={2} sx={{ border: "1px solid lightgrey", borderBottom: "none", mb: 4 }}>
       <Table size="small" aria-label="goalie statistics table">
@@ -10,15 +10,22 @@ function GoalieStats({ stats }) {
             <TableCell sx={{ position: "sticky", left: "0", backgroundColor: "#F7F7F7" }}>Player</TableCell>
             <TableCell align="center">Team</TableCell>
             <TableCell align="center">S/C</TableCell>
-            {stats.columns.map((column) => (
-              <TableCell align="center" key={column.abbreviation}>{column.abbreviation}</TableCell>
-            ))}
+            <TableCell align="center">GP</TableCell>
+            <TableCell align="center">GS</TableCell>
+            <TableCell align="center">W</TableCell>
+            {Boolean(season.saveStatsTracked) && <TableCell align="center">SA</TableCell>}
+            {Boolean(season.saveStatsTracked) && <TableCell align="center">Svs</TableCell>}
+            <TableCell align="center">GA</TableCell>
+            {Boolean(season.saveStatsTracked) && <TableCell align="center">Sv%</TableCell>}
+            <TableCell align="center">GAA</TableCell>
+            <TableCell align="center">SO</TableCell>
+            <TableCell align="center">TOI</TableCell>
           </TableRow>
         </TableHead>
 
         <TableBody>
-          {stats.players.map((player) => (
-            <TableRow key={player.PlayerID}>
+          {stats?.map((player) => (
+            <TableRow key={player.playerId}>
               <TableCell
                 component="th"
                 scope="row"
@@ -32,11 +39,11 @@ function GoalieStats({ stats }) {
                 >
                   <Box
                     component="img"
-                    src={player.ImageURL}
+                    src={player.imageURL}
                     alt=""
                     sx={{ display: { xs: "none", sm: "block" }, width: "50px", borderRadius: "50%", border: "1px solid grey", mr: 1.5 }}
                   />
-                  <Typography variant="body2">{player.Player}</Typography>
+                  <Typography variant="body2">{player.player}</Typography>
                 </Stack>
               </TableCell>
               <TableCell align="center">
@@ -49,16 +56,23 @@ function GoalieStats({ stats }) {
                   <Box
                     component="img"
                     sx={{ width: { xs: "30px", sm: "40px" }, height: "auto", mr: 1 }}
-                    src={player.TeamLogoURL}
+                    src={player.teamLogoURL}
                     alt=""
                   />
-                  <Typography variant="body2">{player.TeamAbbreviation}</Typography>
+                  <Typography variant="body2">{player.teamAbbreviation}</Typography>
                 </Stack>
               </TableCell>
-              <TableCell align="center">{player.Catches}</TableCell>
-              {stats.columns.map((column) => (
-                player[column.property] !== null && <TableCell align="center" key={column.name}>{player[column.property]}</TableCell>
-              ))}
+              <TableCell align="center">{player.catches}</TableCell>
+              <TableCell align="center">{player.gamesPlayed}</TableCell>
+              <TableCell align="center">{player.gamesStarted}</TableCell>
+              <TableCell align="center">{player.wins}</TableCell>
+              {Boolean(season.saveStatsTracked) && <TableCell align="center">{player.shotsAgainst}</TableCell>}
+              {Boolean(season.saveStatsTracked) && <TableCell align="center">{player.saves}</TableCell>}
+              <TableCell align="center">{player.goalsAgainst}</TableCell>
+              {Boolean(season.saveStatsTracked) && <TableCell align="center">{player.savePercentage}</TableCell>}
+              <TableCell align="center">{player.goalsAgainstAverage}</TableCell>
+              <TableCell align="center">{player.shutouts}</TableCell>
+              <TableCell align="center">{player.timeOnIce}</TableCell>
             </TableRow>
           ))}
         </TableBody>
