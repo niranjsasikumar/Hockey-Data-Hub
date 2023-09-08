e83644d357119b9e6a1541dd7bf940a1e359f1d4
niranjsasikumar <sasikumar.niranj@gmail.com>
2023-09-08 12:06:54 -0400

    Add team info service for current teams

--- backend/src/services/team-info-service.js
+++ backend/src/services/team-info-service.js
@@ -1,15 +1,26 @@
+import { fetchDataFromNHLApi, getCurrentSeason } from "../utils/utils.js";
 import connection from "../database/database.js";
 
 export default async function getTeamInfo(season, team) {
-  return await getPastSeasonTeamInfo(season, team);
-}
+  let basicInfoPromise, playoffsPromise, statsPromise, rosterPromise;
 
-async function getPastSeasonTeamInfo(season, team) {
+  if (season === "current") {
+    basicInfoPromise = getCurrentBasicTeamInfo(team);
+    playoffsPromise = getCurrentPlayoffsResult(team);
+    statsPromise = getCurrentTeamStats(team);
+    rosterPromise = getCurrentRoster(team);
+  } else {
+    basicInfoPromise = getBasicTeamInfo(season, team);
+    playoffsPromise = getPlayoffsResult(season, team);
+    statsPromise = getTeamStats(season, team);
+    rosterPromise = getRoster(season, team);
+  }
+
   const [basicInfo, playoffs, stats, roster] = await Promise.all([
-    getBasicTeamInfo(season, team),
-    getPlayoffsResult(season, team),
-    getTeamStats(season, team),
-    getRoster(season, team)
+    basicInfoPromise,
+    playoffsPromise,
+    statsPromise,
+    rosterPromise
   ]);
 
   return {
@@ -18,6 +29,173 @@ async function getPastSeasonTeamInfo(season, team) {
     stats: stats,
     roster: roster
   };
+}
+
+async function getCurrentBasicTeamInfo(team) {
+  const teamInfo = (await fetchDataFromNHLApi(`/teams/${team}`)).teams[0];
+  const { name, abbreviation, conference, division } = teamInfo;
+
+  return {
+    name,
+    logoURL: `https://assets.nhle.com/logos/nhl/svg/${abbreviation}_light.svg`,
+    conference: conference.name,
+    division: division.name
+  };
+}
+
+async function getCurrentPlayoffsResult(team) {
+  let playoffsData = await fetchDataFromNHLApi("/tournaments/playoffs");
+  if (!("rounds" in playoffsData)) return null;
+  playoffsData = playoffsData.rounds;
+
+  const teamData = (await fetchDataFromNHLApi(
+    `/teams/${team}?expand=team.playoffs,team.stats&gameType=P`
+  )).teams[0];
+
+  const { madePlayoffs, inPlayoffs } = teamData.playoffInfo;
+  if (!madePlayoffs) return "Did Not Qualify";
+
+  let wins = teamData.teamStats[0].splits[0].stat.wins;
+  let requiredWins = 0;
+
+  for (let i = 0; i < playoffsData.length - 1; i++) {
+    requiredWins += playoffsData[i].format.numberOfWins;
+    let roundName = playoffsData[i].names.name;
+    if (wins < requiredWins) {
+      return inPlayoffs ? `In ${roundName}` : `Eliminated in ${roundName}`;
+    }
+  }
+
+  requiredWins += playoffsData[playoffsData.length - 1].format.numberOfWins;
+  return `Stanley Cup ${wins === requiredWins ? "Champions" : "Runners-up"}`;
+}
+
+async function getCurrentTeamStats(team) {
+  const divisionId = (await fetchDataFromNHLApi(`/teams/${team}`)).teams[0].division.id;
+  const standings = (await fetchDataFromNHLApi("/standings?expand=standings.record.overall")).records;
+
+  for (const division of standings) {
+    if ("division" in division && division.division.id != divisionId) continue;
+
+    for (const record of division.teamRecords) {
+      if (record.team.id != Number(team)) continue;
+
+      const {
+        divisionRank,
+        leagueRank,
+        points,
+        gamesPlayed,
+        leagueRecord: { wins, losses, ties, ot },
+        goalsScored,
+        goalsAgainst,
+        records: { overallRecords },
+        streak: { streakCode }
+      } = record;
+
+      const [homeRecord, awayRecord, , last10] = overallRecords;
+
+      return {
+        divisionRank: divisionRank ? divisionRank : null,
+        leagueRank,
+        points,
+        gamesPlayed,
+        wins,
+        losses,
+        ties: ties ? ties : null,
+        overtimeLosses: ot ? ot : null,
+        goalsFor: goalsScored,
+        goalsAgainst,
+        difference: goalsScored - goalsAgainst,
+        homeRecord: getRecordString(homeRecord),
+        awayRecord: getRecordString(awayRecord),
+        last10: getRecordString(last10),
+        streak: streakCode
+      };
+    }
+  }
+}
+
+function getRecordString(record) {
+  return `${record.wins}-${record.losses}${"ties" in record ? `-${record.ties}` : ""}${"ot" in record ? `-${record.ot}` : ""}`;
+}
+
+async function getCurrentRoster(team) {
+  const currentSeason = await getCurrentSeason();
+  const teamData = await fetchDataFromNHLApi(`/teams/${team}?expand=team.roster,roster.person`);
+
+  const { abbreviation: teamAbbreviation, roster: { roster } } = teamData.teams[0];
+  const forwards = [];
+  const defensemen = [];
+  const goalies = [];
+
+  for (const player of roster) {
+    const {
+      person: {
+        id,
+        fullName,
+        captain,
+        alternateCaptain,
+        primaryNumber,
+        shootsCatches,
+        height,
+        weight,
+        birthDate,
+      },
+      position: {
+        abbreviation: position,
+        type: positionType
+      }
+    } = player;
+
+    const playerInfo = {
+      name: fullName,
+      imageURL: `https://assets.nhle.com/mugs/nhl/${currentSeason}/${teamAbbreviation}/${id}.png`,
+      captain,
+      alternateCaptain,
+      number: primaryNumber,
+      height,
+      weight,
+      dateOfBirth: getFormattedDate(birthDate),
+      placeOfBirth: getPlaceOfBirth(player.person)
+    };
+
+    if (positionType === "Forward") {
+      playerInfo.position = position;
+      playerInfo.shoots = shootsCatches;
+      forwards.push(playerInfo);
+    } else if (positionType === "Defenseman") {
+      playerInfo.shoots = shootsCatches;
+      defensemen.push(playerInfo);
+    } else if (positionType === "Goalie") {
+      playerInfo.catches = shootsCatches;
+      goalies.push(playerInfo);
+    }
+  }
+
+  return {
+    forwards: forwards,
+    defensemen: defensemen,
+    goalies: goalies
+  };
+}
+
+function getFormattedDate(date) {
+  return new Date(date).toLocaleString("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+    timeZone: "UTC"
+  });
+}
+
+function getPlaceOfBirth(player) {
+  if (!("birthCity" in player)) return "";
+
+  let result = player.birthCity;
+  if (player.birthStateProvince) result += `, ${player.birthStateProvince}`;
+  if (player.birthCountry) result += `, ${player.birthCountry}`;
+
+  return result;
 }
 
 async function getBasicTeamInfo(season, team) {
@@ -54,7 +232,7 @@ async function getPlayoffsResult(season, team) {
   const abbreviation = team1Id === Number(team) ? team1Abbreviation : team2Abbreviation;
 
   if (round === "Stanley Cup Final") {
-    return statusShort.includes(abbreviation) ? "Stanley Cup Champions" : "Stanley Cup Runners-up";
+    return `Stanley Cup ${statusShort.includes(abbreviation) ? "Champions" : "Runners-up"}`;
   } else {
     return `Eliminated in ${round}`;
   }
@@ -130,13 +308,13 @@ async function getGoalies(season, team) {
 }
 
 const rosterQueryColumns = [
-  "player",
+  "player AS name",
   "imageURL",
   "captain",
   "alternateCaptain",
   "number",
   "height",
   "weight",
-  "dateOfBirth",
+  `DATE_FORMAT(dateOfBirth, "%b %e, %Y") AS dateOfBirth`,
   "placeOfBirth"
 ];
