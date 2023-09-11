22116d63531e276bcc3970e04e4d7f9b136a9c49
niranjsasikumar <sasikumar.niranj@gmail.com>
2023-09-10 23:31:10 -0400

    Revert "Revert Scores changes"
    This reverts commit 7423dfa82af8a44e8b0ed35d2e399c1e11b2f4c5.

--- frontend/src/components/pages/scores/sample-scores-data.js
+++ frontend/src/components/pages/scores/sample-scores-data.js
@@ -1,135 +1,5 @@
 export const scores = [
   {
-    date: "Thu, Apr 13, 2023",
-    games: [
-      {
-        ID: 2022021301,
-        Season: 20222023,
-        Type: "Regular Season",
-        DateTime: "2023-04-14T03:00:00.000Z",
-        LastPeriod: "OT",
-        GameStatus: "Final/OT",
-        VenueName: "Capital One Arena",
-        PlayoffRound: null,
-        PlayoffSeriesID: null,
-        PlayoffGameNumber: null,
-        AwayID: 1,
-        AwayName: "Devils",
-        AwayAbbreviation: "NJD",
-        AwayLogoURL: "https://www-league.nhlstatic.com/images/logos/teams-20222023-light/1.svg",
-        AwayGoals: 5,
-        AwayGoalScorers: "Erik Haula (1st, 17:06) | Miles Wood (2nd, 18:36) | Erik Haula (3rd, 03:17) | Dougie Hamilton (3rd, 12:23) | Luke Hughes (OT, 04:33)",
-        HomeID: 15,
-        HomeName: "Capitals",
-        HomeAbbreviation: "WSH",
-        HomeLogoURL: "https://www-league.nhlstatic.com/images/logos/teams-20222023-light/15.svg",
-        HomeGoals: 4,
-        HomeGoalScorers: "Joe Snively (1st, 04:36) | Rasmus Sandin (1st, 05:59) | Craig Smith (1st, 10:18) | Tom Wilson (2nd, 01:05)"
-      },
-      {
-        ID: 2022021302,
-        Season: 20222023,
-        Type: "Regular Season",
-        DateTime: "2023-04-14T03:00:00.000Z",
-        LastPeriod: "OT",
-        GameStatus: "Final/OT",
-        VenueName: "KeyBank Center",
-        PlayoffRound: null,
-        PlayoffSeriesID: null,
-        PlayoffGameNumber: null,
-        AwayID: 9,
-        AwayName: "Senators",
-        AwayAbbreviation: "OTT",
-        AwayLogoURL: "https://www-league.nhlstatic.com/images/logos/teams-20222023-light/9.svg",
-        AwayGoals: 3,
-        AwayGoalScorers: "Tim Stützle (2nd, 04:27) | Dylan Gambrell (2nd, 06:11) | Claude Giroux (2nd, 12:36)",
-        HomeID: 7,
-        HomeName: "Sabres",
-        HomeAbbreviation: "BUF",
-        HomeLogoURL: "https://www-league.nhlstatic.com/images/logos/teams-20222023-light/7.svg",
-        HomeGoals: 4,
-        HomeGoalScorers: "Henri Jokiharju (2nd, 00:43) | Victor Olofsson (2nd, 02:46) | Tage Thompson (3rd, 03:03) | Casey Mittelstadt (OT, 01:18)"
-      },
-      {
-        ID: 2022021303,
-        Season: 20222023,
-        Type: "Regular Season",
-        DateTime: "2023-04-14T03:00:00.000Z",
-        LastPeriod: "OT",
-        GameStatus: "Final/OT",
-        VenueName: "Nationwide Arena",
-        PlayoffRound: null,
-        PlayoffSeriesID: null,
-        PlayoffGameNumber: null,
-        AwayID: 5,
-        AwayName: "Penguins",
-        AwayAbbreviation: "PIT",
-        AwayLogoURL: "https://www-league.nhlstatic.com/images/logos/teams-20222023-light/5.svg",
-        AwayGoals: 2,
-        AwayGoalScorers: "Kris Letang (1st, 04:28) | Jake Guentzel (3rd, 03:53)",
-        HomeID: 29,
-        HomeName: "Blue Jackets",
-        HomeAbbreviation: "CBJ",
-        HomeLogoURL: "https://www-league.nhlstatic.com/images/logos/teams-20222023-light/29.svg",
-        HomeGoals: 3,
-        HomeGoalScorers: "Andrew Peeke (2nd, 02:12) | Emil Bemstrom (3rd, 16:35) | Johnny Gaudreau (OT, 01:00)"
-      },
-      {
-        ID: 2022021304,
-        Season: 20222023,
-        Type: "Regular Season",
-        DateTime: "2023-04-14T03:00:00.000Z",
-        LastPeriod: "3rd",
-        GameStatus: "Final",
-        VenueName: "Madison Square Garden",
-        PlayoffRound: null,
-        PlayoffSeriesID: null,
-        PlayoffGameNumber: null,
-        AwayID: 10,
-        AwayName: "Maple Leafs",
-        AwayAbbreviation: "TOR",
-        AwayLogoURL: "https://www-league.nhlstatic.com/images/logos/teams-20222023-light/10.svg",
-        AwayGoals: 3,
-        AwayGoalScorers: "Timothy Liljegren (3rd, 03:09) | William Nylander (3rd, 07:48) | Noel Acciari (3rd, 12:21)",
-        HomeID: 3,
-        HomeName: "Rangers",
-        HomeAbbreviation: "NYR",
-        HomeLogoURL: "https://www-league.nhlstatic.com/images/logos/teams-20222023-light/3.svg",
-        HomeGoals: 2,
-        HomeGoalScorers: "Kaapo Kakko (2nd, 01:38) | Artemi Panarin (3rd, 06:26)"
-      }
-    ]
-  },
-  {
-    date: "Thu, May 18, 2023",
-    games: [
-      {
-        ID: 2022030311,
-        Season: 20222023,
-        Type: "P",
-        DateTime: "2023-05-19T04:00:00.000Z",
-        LastPeriod: "4OT",
-        GameStatus: "Final/4OT",
-        VenueName: "PNC Arena",
-        PlayoffRound: "Conference Finals",
-        PlayoffSeriesID: 2022202331,
-        PlayoffGameNumber: "Game 1",
-        AwayID: 13,
-        AwayName: "Panthers",
-        AwayAbbreviation: "FLA",
-        AwayLogoURL: "https://www-league.nhlstatic.com/images/logos/teams-20222023-light/13.svg",
-        AwayGoals: 3,
-        AwayGoalScorers: "Aleksander Barkov (2nd, 15:28) | Carter Verhaeghe (2nd, 17:43) | Matthew Tkachuk (4OT, 19:47)",
-        HomeID: 12,
-        HomeName: "Hurricanes",
-        HomeAbbreviation: "CAR",
-        HomeLogoURL: "https://www-league.nhlstatic.com/images/logos/teams-20222023-light/12.svg",
-        HomeGoals: 2,
-        HomeGoalScorers: "Seth Jarvis (1st, 19:48) | Stefan Noesen (3rd, 03:47)"
-      }
-    ]
-  },
-  {
     date: "Tue, Jun 13, 2023",
     games: [
       {
@@ -144,26 +14,17 @@ export const scores = [
         PlayoffSeriesID: 2022202341,
         PlayoffGameNumber: "Game 5",
         AwayID: 13,
-<<<<<<< HEAD
         AwayName: "Florida Panthers",
-=======
-        AwayName: "Panthers",
->>>>>>> parent of 90585fb (Modify sample data on Scores page)
         AwayAbbreviation: "FLA",
         AwayLogoURL: "https://www-league.nhlstatic.com/images/logos/teams-20222023-light/13.svg",
         AwayGoals: 3,
         AwayGoalScorers: "Aaron Ekblad (2nd, 02:15) | Sam Reinhart (3rd, 08:47) | Sam Bennett (3rd, 11:39)",
         HomeID: 54,
-<<<<<<< HEAD
         HomeName: "Vegas Golden Knights",
-=======
-        HomeName: "Golden Knights",
->>>>>>> parent of 90585fb (Modify sample data on Scores page)
         HomeAbbreviation: "VGK",
         HomeLogoURL: "https://www-league.nhlstatic.com/images/logos/teams-20222023-light/54.svg",
         HomeGoals: 9,
         HomeGoalScorers: "Mark Stone (1st, 11:52) | Nicolas Hague (1st, 13:41) | Alec Martinez (2nd, 10:28) | Reilly Smith (2nd, 12:13) | Mark Stone (2nd, 17:15) | Michael Amadio (2nd, 19:58) | Ivan Barbashev (3rd, 08:22) | Mark Stone (3rd, 14:06) | Nicolas Roy (3rd, 18:58)"
-<<<<<<< HEAD
       }
     ]
   },
@@ -222,8 +83,6 @@ export const scores = [
         HomeLogoURL: "https://www-league.nhlstatic.com/images/logos/teams-20222023-light/13.svg",
         HomeGoals: 3,
         HomeGoalScorers: "Brandon Montour (1st, 04:08) | Matthew Tkachuk (3rd, 17:47) | Carter Verhaeghe (OT, 04:27)"
-=======
->>>>>>> parent of 90585fb (Modify sample data on Scores page)
       }
     ]
   }
