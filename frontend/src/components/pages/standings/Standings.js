31f35ca5698a2e7b78ebaf9299afcc6b3088d9ed
niranjsasikumar <sasikumar.niranj@gmail.com>
2023-09-10 23:32:03 -0400

    Revert "Modify Season field in Standings page"
    This reverts commit 88fae3a9c13e80007f2d2855a2ff1686d33b70ac.

--- frontend/src/components/pages/standings/Standings.js
+++ frontend/src/components/pages/standings/Standings.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { seasonConference, standingsConference, playoffs } from "./sample-standings-data";
+import { seasonConference, standingsConference, seasonDivision, standingsDivision, seasonLeague, standingsLeague, playoffs } from "./sample-standings-data";
 import { Container, Typography, Divider, FormControl, InputLabel, Select, MenuItem, Box, Grid } from "@mui/material";
 import StandingsConference from "./StandingsConference";
 import StandingsDivision from "./StandingsDivision";
@@ -10,6 +10,14 @@ const seasons = {
   20222023: {
     seasonInfo: seasonConference,
     standings: standingsConference
+  },
+  20212022: {
+    seasonInfo: seasonDivision,
+    standings: standingsDivision
+  },
+  20202021: {
+    seasonInfo: seasonLeague,
+    standings: standingsLeague
   }
 }
 
@@ -33,13 +41,15 @@ function Standings() {
   }, [season]);
 
   return(
-    <Container maxWidth={false} sx={{ maxWidth: "1300px", pt: 3, pb: 5 }}>
+    <Container maxWidth={false} sx={{ maxWidth: "1300px", py: 3 }}>
       <Typography variant="h3" component="h1">Standings</Typography>
       <Divider sx={{ mt: 2, mb: 3 }} />
       <FormControl sx={{ mr: 2, mb: 2, minWidth: 140 }}>
         <InputLabel id="season-label">Season</InputLabel>
         <Select labelId="season-label" id="season" value={season} label="Season" onChange={handleSeasonChange}>
           <MenuItem value={20222023}>2022-2023</MenuItem>
+          <MenuItem value={20212022}>2021-2022</MenuItem>
+          <MenuItem value={20202021}>2020-2021</MenuItem>
         </Select>
       </FormControl>
       <FormControl sx={{ mb: 2, minWidth: 140 }}>
