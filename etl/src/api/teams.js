bba16d78e87ac04f57ba5c4b024aa24ac70feb9a
niranjsasikumar <sasikumar.niranj@gmail.com>
2023-10-01 10:25:18 -0400

    Add ETL tests for teams.js

--- etl/src/api/teams.js
+++ etl/src/api/teams.js
@@ -1,8 +1,21 @@
-import { getLogoUrl, fetchDataFromApi } from "./api.js";
+import { fetchDataFromApi, getLogoUrl } from "./api.js";
 import { LAST_SEASON } from "../constants.js";
 
+/* Convert data of current teams from NHL API to rows of values to insert into
+"teams" table */
+export async function getTeamValues() {
+  const teamsData = (await fetchDataFromApi("/teams")).teams;
+  const teamValues = [];
+
+  for (const team of teamsData) {
+    teamValues.push(await extractTeamData(team));
+  }
+
+  return teamValues;
+}
+
 // Returns a row of values to insert into "teams" table
-async function extractTeamData(team) {
+export async function extractTeamData(team) {
   const logoUrl = await getLogoUrl(LAST_SEASON, team.id);
   return [
     team.id,
@@ -18,17 +31,4 @@ async function extractTeamData(team) {
     team.division?.name,
     null
   ];
-}
-
-/* Convert data of current teams from NHL API to rows of values to insert into
-"teams" table */
-export async function getTeamValues() {
-  const teamsData = (await fetchDataFromApi("/teams")).teams;
-  const teamValues = [];
-
-  for (const team of teamsData) {
-    teamValues.push(await extractTeamData(team));
-  }
-
-  return teamValues;
 }
