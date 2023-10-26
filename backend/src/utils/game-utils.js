e5d91d5778511798b18156b1815e256a8cdad452
niranjsasikumar <sasikumar.niranj@gmail.com>
2023-10-26 14:19:59 -0400

    Add backend tests for game-utils.js

--- backend/src/utils/game-utils.js
+++ backend/src/utils/game-utils.js
@@ -1,33 +1,53 @@
-import { getLogoUrl } from "./utils.js";
-
 // Extract the required game data from the response received from the NHL API
 export function extractGames(scheduleData) {
   const games = [];
 
   for (const date of scheduleData) {
     for (const game of date.games) {
+      const { gamePk, gameType, gameDate, status: { statusCode } } = game;
+      const round = game.seriesSummary?.series?.round?.names?.name;
+      const gameLabel = game.seriesSummary?.gameLabel;
       const { away, home } = game.teams;
-      const awayAbbreviation = away.team?.abbreviation;
-      const homeAbbreviation = home.team?.abbreviation;
+      const {
+        teamName: awayShortName, abbreviation: awayAbbreviation
+      } = away.team;
+      const {
+        teamName: homeShortName, abbreviation: homeAbbreviation
+      } = home.team;
+
+      const playoffRound = round ? round : null;
+      const playoffGameNumber = gameLabel ? gameLabel : null;
+      const awayLogoURL = (
+        `https://assets.nhle.com/logos/nhl/svg/${awayAbbreviation}_light.svg`
+      );
+      const homeLogoURL = (
+        `https://assets.nhle.com/logos/nhl/svg/${homeAbbreviation}_light.svg`
+      );
+      const awayGoals = ["3", "4", "5", "6", "7"].includes(statusCode)
+        ? away.score
+        : null;
+      const homeGoals = ["3", "4", "5", "6", "7"].includes(statusCode)
+        ? home.score
+        : null;
       const [awayGoalScorers, homeGoalScorers] = getGoalScorers(game);
 
       games.push({
-        id: game.gamePk,
-        type: gameTypes[game.gameType],
-        dateTime: game.gameDate,
+        id: gamePk,
+        type: gameTypes[gameType],
+        dateTime: gameDate,
         status: getGameStatus(game),
-        playoffRound: "seriesSummary" in game ? game.seriesSummary?.series?.round?.names?.name : null,
-        playoffGameNumber: game.seriesSummary?.gameLabel,
-        awayShortName: away.team?.teamName,
-        awayAbbreviation: awayAbbreviation,
-        awayLogoURL: `https://assets.nhle.com/logos/nhl/svg/${awayAbbreviation}_light.svg`,
-        awayGoals: ["3", "4", "5", "6", "7"].includes(game.status.statusCode) ? away.score : null,
-        awayGoalScorers: awayGoalScorers,
-        homeShortName: home.team?.teamName,
-        homeAbbreviation: homeAbbreviation,
-        homeLogoURL: `https://assets.nhle.com/logos/nhl/svg/${homeAbbreviation}_light.svg`,
-        homeGoals: ["3", "4", "5", "6", "7"].includes(game.status.statusCode) ? home.score : null,
-        homeGoalScorers: homeGoalScorers
+        playoffRound,
+        playoffGameNumber,
+        awayShortName,
+        awayAbbreviation,
+        awayLogoURL,
+        awayGoals,
+        awayGoalScorers,
+        homeShortName,
+        homeAbbreviation,
+        homeLogoURL,
+        homeGoals,
+        homeGoalScorers
       });
     }
   }
@@ -35,7 +55,7 @@ export function extractGames(scheduleData) {
   return games;
 }
 
-function getGoalScorers(game) {
+export function getGoalScorers(game) {
   let awayId = game.teams?.away?.team?.id;
   let awayGoalScorers = "";
   let homeGoalScorers = "";
@@ -45,24 +65,31 @@ function getGoalScorers(game) {
       if (player.playerType === "Scorer") {
         const playerName = player.player?.fullName;
         if (goal.team?.id === awayId) {
-          awayGoalScorers += awayGoalScorers === "" ? playerName : " | " + playerName;
+          awayGoalScorers += (awayGoalScorers ? " | " : "") + playerName;
         } else {
-          homeGoalScorers += homeGoalScorers === "" ? playerName : " | " + playerName;
+          homeGoalScorers += (homeGoalScorers ? " | " : "") + playerName;
         }
         break;
       }
     }
 
-    const goalInfo = " (" + goal.about?.ordinalNum + ", " + goal.about?.periodTime + ")";
-    goal.team?.id === awayId ? awayGoalScorers += goalInfo : homeGoalScorers += goalInfo;
+    const goalInfo = " (" + goal.about?.ordinalNum + ", "
+      + goal.about?.periodTime + ")";
+    
+    if (goal.team?.id === awayId) awayGoalScorers += goalInfo;
+    else homeGoalScorers += goalInfo;
   }
 
   if (awayGoalScorers === "") {
-    awayGoalScorers = game.teams?.away?.score > 0 ? "Goal scorer information not available" : "No goals";
+    awayGoalScorers = game.teams?.away?.score > 0
+      ? "Goal scorer information not available"
+      : "No goals";
   }
 
   if (homeGoalScorers === "") {
-    homeGoalScorers = game.teams?.home?.score > 0 ? "Goal scorer information not available" : "No goals";
+    homeGoalScorers = game.teams?.home?.score > 0
+      ? "Goal scorer information not available"
+      : "No goals";
   }
 
   return [awayGoalScorers, homeGoalScorers];
@@ -76,19 +103,26 @@ export const gameTypes = {
   WA: "Women's All-Star Game"
 };
 
-function getGameStatus(game) {
+export function getGameStatus(game) {
+  const {
+    currentPeriodOrdinal: currentPeriod,
+    currentPeriodTimeRemaining
+  } = game.linescore;
+
   switch (game.status.statusCode) {
     case "1":
     case "2":
       return "Scheduled";
     case "3":
     case "4":
-      return `${game.linescore?.currentPeriodOrdinal}, ${game.linescore?.currentPeriodTimeRemaining}`;
+      return `${currentPeriod}, ${currentPeriodTimeRemaining}`;
     case "5":
     case "6":
     case "7":
-      const currentPeriod = game.linescore?.currentPeriodOrdinal;
-      return currentPeriod === "3rd" || currentPeriod === "2nd" ? "Final" : "Final/" + currentPeriod;
+      let status;
+      if (currentPeriod === "3rd" || currentPeriod === "2nd") status = "Final";
+      else status = "Final/" + currentPeriod
+      return status;
     case "8":
       return "Time TBD";
     case "9":
---
+++ backend/test/utils/game-utils.test.js
@@ -0,0 +1,68 @@
+import {
+  extractGames, getGoalScorers, getGameStatus
+} from "../../src/utils/game-utils";
+import scheduleData from "../test_data/extractGames.json";
+import gamesGoalInfo from "../test_data/getGoalScorers.json";
+import gamesStatus from "../test_data/getGameStatus.json";
+
+test("extractGames", () => {
+  const games = extractGames(scheduleData);
+  expect(games.length).toBe(2);
+  expect(games[0].playoffRound).toBeNull();
+  expect(games[0].playoffGameNumber).toBeNull();
+  expect(games[0].awayGoals).toBeNull();
+  expect(games[0].homeGoals).toBeNull();
+  expect(games[1].playoffRound).toBe("Stanley Cup Final");
+  expect(games[1].playoffGameNumber).toBe("Game 5");
+  expect(games[1].awayGoals).toBe(3);
+  expect(games[1].homeGoals).toBe(9);
+});
+
+describe("getGoalScorers", () => {
+  test("No goals for away team", () => {
+    const game = gamesGoalInfo.noAwayGoals;
+    const [awayGoalScorers, homeGoalScorers] = getGoalScorers(game);
+    expect(awayGoalScorers).toBe("No goals");
+    expect(homeGoalScorers).not.toBe("No goals");
+  });
+
+  test("No goals for home team", () => {
+    const game = gamesGoalInfo.noHomeGoals;
+    const [awayGoalScorers, homeGoalScorers] = getGoalScorers(game);
+    expect(awayGoalScorers).not.toBe("No goals");
+    expect(homeGoalScorers).toBe("No goals");
+  });
+
+  test("Goal scorer information not available", () => {
+    const game = gamesGoalInfo.noGoalScorerInfo;
+    const [awayGoalScorers, homeGoalScorers] = getGoalScorers(game);
+    expect(awayGoalScorers).toBe("Goal scorer information not available");
+    expect(homeGoalScorers).toBe("Goal scorer information not available");
+  });
+});
+
+describe("getGameStatus", () => {
+  function testGetGameStatus(statusCode, expectedStatus) {
+    test(`Game status ${statusCode}`, () => {
+      const game = gamesStatus[statusCode - 1];
+      const status = getGameStatus(game);
+      expect(status).toBe(expectedStatus);
+    });
+  }
+
+  const expectedStatuses = [
+    "Scheduled",
+    "Scheduled",
+    "2nd, 10:00",
+    "3rd, 1:00",
+    "Final",
+    "Final",
+    "Final/OT",
+    "Time TBD",
+    "Postponed"
+  ];
+
+  for (let i = 1; i <= 9; i++) {
+    testGetGameStatus(i, expectedStatuses[i-1]);
+  }
+});
